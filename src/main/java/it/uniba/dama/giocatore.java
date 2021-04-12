9afe95c697360e0f84548c4695313cda0224f3d5
Citrone16 <michelefraccalvieri1601@gmail.com>
2021-04-12 18:17:23 +0200

    Creata classe giocatore

--- src/main/java/it/uniba/dama/giocatore.java
+++ src/main/java/it/uniba/dama/giocatore.java
@@ -3,7 +3,7 @@ package it.uniba.dama;
 public class giocatore {
     private String colore;
 
-    public giocatore(String colore){
+    public giocatore(String colore) {
         this.colore = colore;
     }
 }
