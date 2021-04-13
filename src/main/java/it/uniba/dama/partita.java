ccec489f7a8761177730f7ef4ebcf82ceb24d4ae
FDigre <f.digregorio8@studenti.uniba.it>
2021-04-13 17:14:30 +0200

    Creato metodo abbandona

--- src/main/java/it/uniba/dama/partita.java
+++ src/main/java/it/uniba/dama/partita.java
@@ -2,12 +2,16 @@ package it.uniba.dama;
 
 import it.uniba.dama.giocatore;
 import it.uniba.dama.damiera;
+import it.uniba.main.AppMain;
+import java.util.Scanner;
+
 
 public class partita {
     private giocatore bianco;
     private giocatore nero;
     private damiera tavolo;
     private boolean inCorso;
+    private String turno;
 
     public partita() {
         tavolo = new damiera();
@@ -27,6 +31,28 @@ public class partita {
         nero = new giocatore("nero");
         tavolo.popolaDamiera();
         inCorso = true;
+        turno = "bianco";
+
+        
+    }
+
+    public void abbandona() {
+        Scanner inputTastiera = new Scanner(System.in);
+        String conferma;
+        do {
+            System.out.print("Sei sicuro di voler abbandonare? (si/no) ");
+            conferma = inputTastiera.nextLine();
+            if (!conferma.equals("si") && !conferma.equals("no"))
+                System.out.println("Comando inesistente.");
+        } while (!conferma.equals("si") && !conferma.equals("no"));
+
+        if (conferma.equals("si")) {
+            inCorso = false;
+            if (turno.equals("bianco"))
+                System.out.println("Partita abbandonata: il nero ha vinto.");
+            else
+                System.out.println("Partita abbandonata: il bianco ha vinto.");
+        }
     }
 
 }
