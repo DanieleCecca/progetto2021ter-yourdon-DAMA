6a2fa450adfaa3803674fcadb7fec08312f05f7c
Citrone16 <michelefraccalvieri1601@gmail.com>
2021-04-24 16:35:04 +0200

    Creata classe Dama

---
+++ src/main/java/it/uniba/dama/Dama.java
@@ -0,0 +1,11 @@
+package it.uniba.dama;
+
+import it.uniba.dama.Pedina;
+
+public class Dama extends Pedina {
+    public Dama(){};
+
+    public Dama (String colore, String simbolo) {
+        super(colore, simbolo);
+    }
+}
