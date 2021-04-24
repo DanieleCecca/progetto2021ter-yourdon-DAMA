7a545cc41d788c1254b383177c415a17a5eea412
FDigre <f.digregorio8@studenti.uniba.it>
2021-04-24 16:51:10 +0200

    Creata classe Costanti

---
+++ src/main/java/it/uniba/utilita/Costanti.java
@@ -0,0 +1,21 @@
+package it.uniba.utilita;
+
+import it.uniba.dama.Pedina;
+import it.uniba.dama.Dama;
+
+public class Costanti {
+    public static final Pedina PEDINA_BIANCA = new Pedina("bianco", "\u26C2");
+    public static final Pedina PEDINA_NERA = new Pedina("nero", "\u26C0");
+
+    public static final Dama DAMA_BIANCA = new Dama("bianco", "\u26C1");
+    public static final Dama DAMA_NERA = new Dama("nero", "\u26C3");
+
+    public static final String UNICODE_PEDINA_BIANCA = "\u26C2";
+    public static final String UNICODE_PEDINA_NERA = "\u26C0";
+    public static final String UNICODE_DAMA_BIANCA = "\u26C1";
+    public static final String UNICODE_DAMA_NERA = "\u26C3";
+
+    public static final String SPAZIO_CORTO = "\u202F";
+
+    public static final int DIM = 8;
+}
