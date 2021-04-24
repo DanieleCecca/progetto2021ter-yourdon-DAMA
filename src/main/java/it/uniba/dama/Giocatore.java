902560a8b2b7248ec3152b05e02851244e241d69
Ferrulli Francesco <scuola.franferr@gmail.com>
2021-04-24 18:06:29 +0200

    Modificata classe Giocatore

--- src/main/java/it/uniba/dama/Giocatore.java
+++ src/main/java/it/uniba/dama/Giocatore.java
@@ -1,10 +1,32 @@
 package it.uniba.dama;
 
+import it.uniba.utilita.Cronometro;
+
 public class Giocatore {
     private String colore;
+    private  Cronometro cronometro;
+    private int tempoPrecedente;
 
     public Giocatore(String colore) {
         this.colore = colore;
+        this.cronometro = new Cronometro(tempoPrecedente);
+        tempoPrecedente = 0;
+    }
+
+    public Cronometro getCronometro() {
+        return cronometro;
+    }
+
+    public void setCronometro(Cronometro cronometro) {
+        this.cronometro = cronometro;
+    }
+
+    public int getTempoPrecedente() {
+        return tempoPrecedente;
+    }
+
+    public void setTempoPrecedente(int tempoPrecedente) {
+        this.tempoPrecedente = tempoPrecedente;
     }
 
     public String getColore() {
