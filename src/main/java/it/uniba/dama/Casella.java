6d03aa4ea2e3c4dc3fd6ba0459f28d1ac4d1f101
Daniele Cecca <d.cecca1@studenti.uniba.it>
2021-04-25 19:24:36 +0200

    Aggiunta classe Coordinate

--- src/main/java/it/uniba/dama/Casella.java
+++ src/main/java/it/uniba/dama/Casella.java
@@ -4,9 +4,19 @@ public class Casella {
     private Pedina pedina;
     private boolean occupato;
     private int numeroCasella;
+    private Coordinate coordinate;
+
+    public Coordinate getCordinate() {
+        return coordinate;
+    }
+
+    public void setCordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
 
     public Casella (){
         pedina = new Pedina("", "");
+        coordinate = new Coordinate();
     }
 
     public int getNumeroCasella() {
@@ -35,5 +45,29 @@ public class Casella {
 
     public void setOccupato(boolean occupato) {
         this.occupato = occupato;
+    }
+}
+
+ class Coordinate {
+    private int x;
+    private int y;
+
+    Coordinate(){
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
