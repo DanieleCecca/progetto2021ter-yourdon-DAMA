0f52b5ada235a1f26a83071fab0d69cd3dab8669
Ferrulli Francesco <scuola.franferr@gmail.com>
2021-05-17 18:26:57 +0200

    Corretti errori di checkstyle in Cronometro

--- src/main/java/it/uniba/utilita/Cronometro.java
+++ src/main/java/it/uniba/utilita/Cronometro.java
@@ -14,11 +14,11 @@ package it.uniba.utilita;
  * 		    <li>Calcola il tempo in run-time</li>
  * 		    <li>Stampa il tempo trascorso</li></ul>
  */
-public class Cronometro extends Thread {
+public final class Cronometro extends Thread {
     private int secondi = 0;
     private boolean continua = true;
 
-    public Cronometro(int tempoPrecedente) {
+    public Cronometro(final int tempoPrecedente) {
         secondi = tempoPrecedente;
     }
 
@@ -26,22 +26,22 @@ public class Cronometro extends Thread {
         return secondi;
     }
 
-    public void setSecondi(int secondi) {
-        this.secondi = secondi;
+    public void setSecondi(final int sec) {
+        this.secondi = sec;
     }
 
     public boolean getContinua() {
         return continua;
     }
 
-    public void setContinua(boolean continua) {
-        this.continua = continua;
+    public void setContinua(final boolean cont) {
+        this.continua = cont;
     }
 
     public void run() {
         try {
             while (continua) {
-                Thread.sleep(1000);
+                Thread.sleep(Costanti.SECONDO);
                 secondi++;
             }
         } catch (InterruptedException e) {
@@ -49,13 +49,13 @@ public class Cronometro extends Thread {
         }
     }
 
-    public void stampaTempoTrascorso() {
-        int secondi, minuti, ore;
+    public String tempoTrascorso() {
+        int sec, minuti, ore;
 
-        secondi = this.secondi;
-        ore = secondi / 3600;
-        minuti = (secondi % 3600) / 60;
-        secondi = (secondi % 3600) % 60;
-        Interfaccia.stampaMessaggio(ore + " h " + minuti + " min " + secondi + " sec ");
+        sec = this.secondi;
+        ore = sec / Costanti.ORA_IN_SECONDI;
+        minuti = (sec % Costanti.ORA_IN_SECONDI) / Costanti.MINUTO_IN_SECONDI;
+        sec = (sec % Costanti.ORA_IN_SECONDI) % Costanti.MINUTO_IN_SECONDI;
+        return (ore + " h " + minuti + " min " + sec + " sec ");
     }
 }
