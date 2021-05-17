b05837268f14ccf532b965a9097dc21f5a46f4ba
Daniele Cecca <d.cecca1@studenti.uniba.it>
2021-05-17 20:21:03 +0200

    Coretti errori checkstyle in Dama

--- src/main/java/it/uniba/dama/Dama.java
+++ src/main/java/it/uniba/dama/Dama.java
@@ -1,24 +1,23 @@
 package it.uniba.dama;
 
-import it.uniba.dama.Pedina;
-
 /**
  * Sottoclasse di Pedina che rappresenta la Dama<br>
  * Tipo di classe: <b>Entity</b><br>
  * Responsabilità:
- * 		<ul>Knows:
- *          <li>Il colore relativo alla dama</li>
- *          <li>Il simbolo relativo alla dama(in UNICODE)</li>
- *      </ul>
- *      <ul>Does:
- *          <li>Istanzia il colore e simbolo della dama utilizzando il costruttore della superclasse</li>
- *      </ul>
+ * <ul>Knows:
+ *        <li>Il colore relativo alla dama</li>
+ *        <li>Il simbolo relativo alla dama(in UNICODE)</li>
+ *    </ul>
+ *    <ul>Does:
+ *        <li>Istanzia il colore e simbolo della dama utilizzando il costruttore della superclasse</li>
+ *    </ul>
  */
 
 public class Dama extends Pedina {
-    public Dama(){};
+    public Dama() {
+    }
 
-    public Dama (String colore, String simbolo) {
+    public Dama(final String colore, final String simbolo) {
         super(colore, simbolo);
     }
 }
