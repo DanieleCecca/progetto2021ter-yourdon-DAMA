74bdda6c6afc2e847c4493fc82729a1f1dcb82e1
FDigre <f.digregorio8@studenti.uniba.it>
2021-05-25 16:06:49 +0200

    Eliminati import inutilizzati di PedinaTest

--- src/test/java/it/uniba/dama/PedinaTest.java
+++ src/test/java/it/uniba/dama/PedinaTest.java
@@ -1,18 +1,10 @@
 package it.uniba.dama;
 
-import it.uniba.dama.Giocatore;
-import it.uniba.dama.Pedina;
 import it.uniba.utilita.Costanti;
-import it.uniba.utilita.Cronometro;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class PedinaTest {
 
