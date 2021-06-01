523dbb2e93583bfd6ccc36c2e9fdd779d9f1df61
Ferrulli Francesco <scuola.franferr@gmail.com>
2021-06-02 01:33:23 +0200

    Corretti errori di checkstyle in DamaTest

--- src/test/java/it/uniba/dama/DamaTest.java
+++ src/test/java/it/uniba/dama/DamaTest.java
@@ -2,23 +2,29 @@ package it.uniba.dama;
 
 import it.uniba.utilita.Costanti;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DamaTest {
+/**
+ * Classe che rappresenta i casi di test della classe Dama
+ */
+public final class DamaTest {
 
     //Test dama
     @Test
     @DisplayName("1: Test assertNotNull Dama")
-    public void DamaTest_1(){
+    public void damaTest1() {
         Dama dama = new Dama();
         assertNotNull(dama);
     }
 
     @Test
     @DisplayName("2: Test assertNotNull Dama")
-    public void DamaTest_2(){
+    public void damaTest2() {
         Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         assertNotNull(dama);
     }
@@ -26,36 +32,36 @@ public class DamaTest {
     //Test sul campo colore
     @Test
     @DisplayName("3: Test assertNotEquals getColore")
-    public void DamaTest_3(){
+    public void damaTest3() {
         Dama dama = new Dama();
         assertNotEquals("bianco", dama.getColore());
     }
 
     @Test
     @DisplayName("4: Test assertEquals getColore")
-    public void DamaTest_4(){
-        Dama dama = new Dama("bianco",Costanti.UNICODE_PEDINA_BIANCA);
+    public void damaTest4() {
+        Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         assertEquals("bianco", dama.getColore());
     }
 
     @Test
     @DisplayName("5: Test assertNotNull getColore")
-    public void DamaTest_5(){
+    public void damaTest5() {
         Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         assertNotNull(dama.getColore());
     }
 
     @Test
     @DisplayName("6: Test assertEquals setColore")
-    public void DamaTest_6(){
-        Dama dama = new Dama("bianco",Costanti.UNICODE_PEDINA_BIANCA);
+    public void damaTest6() {
+        Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         dama.setColore("nero");
         assertEquals("nero", dama.getColore());
     }
 
     @Test
     @DisplayName("7: Test assertNotNull setColore")
-    public void DamaTest_7(){
+    public void damaTest7() {
         Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         dama.setColore("nero");
         assertNotNull(dama.getColore());
@@ -64,7 +70,7 @@ public class DamaTest {
     //Test sul campo simbolo
     @Test
     @DisplayName("8: Test assertNotEquals getSimbolo")
-    public void DamaTest_8(){
+    public void damaTest8() {
         Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         dama.setSimbolo(Costanti.UNICODE_PEDINA_NERA);
         assertNotEquals(Costanti.UNICODE_PEDINA_BIANCA, dama.getSimbolo());
@@ -72,30 +78,30 @@ public class DamaTest {
 
     @Test
     @DisplayName("9: Test assertEquals getSimbolo")
-    public void DamaTest_9(){
-        Dama dama = new Dama("bianco",Costanti.UNICODE_PEDINA_BIANCA);
+    public void damaTest9() {
+        Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         dama.setSimbolo(Costanti.UNICODE_PEDINA_BIANCA);
         assertEquals("bianco", dama.getColore());
     }
 
     @Test
     @DisplayName("10: Test assertNotNull getSimbolo")
-    public void DamaTest_10(){
+    public void damaTest10() {
         Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         assertNotNull(dama.getSimbolo());
     }
 
     @Test
     @DisplayName("11: Test assertEquals setSimbolo")
-    public void DamaTest_11(){
-        Dama dama = new Dama("bianco",Costanti.UNICODE_PEDINA_BIANCA);
+    public void damaTest11() {
+        Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         dama.setSimbolo("nero");
         assertEquals("nero", dama.getSimbolo());
     }
 
     @Test
     @DisplayName("12: Test assertNotNull setSimbolo")
-    public void DamaTest_12(){
+    public void damaTest12() {
         Dama dama = new Dama("bianco", Costanti.UNICODE_PEDINA_BIANCA);
         dama.setSimbolo("nero");
         assertNotNull(dama.getSimbolo());
